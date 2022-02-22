a84053104e085d45a7a82289a1c0ae2cc62ac87c
taufiqsubki <99981317+taufiqsubki@users.noreply.github.com>
2022-02-23 02:09:10 +0800

    Add files via upload

---
+++ day4_Java_stdin_Stdout_II.java
@@ -0,0 +1,21 @@
+package Taufik;
+import java.util.Scanner;
+
+public class day4_Java_stdin_Stdout_II {
+    
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
